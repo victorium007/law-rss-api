94f04b89a6f12028de926efe117f09a913ea85a5
victorium007 <victorium007@gmail.com>
2025-06-18 15:45:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import feedparser
+import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/search', methods=['GET'])
 def search_law_rss():
@@ -19,4 +21,10 @@ def search_law_rss():
                 "link": entry.link
             })
 
-    return jsonify(results[:5])  # נחזיר עד 5 תוצאות
+    response = jsonify(results[:5])
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
